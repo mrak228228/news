c8f0f6da3d449b22313ce31a4e4255556ad71267
mrak228228 <tima.filyaev@mail.ru>
2025-09-03 15:47:56 +0300

    user authentication

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 AUTH_USER_MODEL = "accounts.CustomUser"
 
+LOGOUT_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = "home"
+
 
 # Application definition
 
@@ -56,7 +59,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
